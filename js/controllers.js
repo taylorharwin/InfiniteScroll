4dab22c102deb18b91b7f301cd60c5dfc434b496
Taylor Harwin <taylor.harwin@gmail.com>
2014-07-30 01:28:45 -0700

    registers event at bottom of scroll

--- js/controllers.js
+++ js/controllers.js
@@ -3,13 +3,16 @@
 
 angular.module('ScrollTest.controllers', [])
 
+// This app has only one controller. Note that the Scroller directive has its own isolate scope, but can also reach up to functions in this scope as well
+
 .controller('MainCtrl', function ($scope) {
+
+  // Mock Data created for the purpose of this exercise. Each of these is passed to a different instance of the infinite scroll controller
 
   $scope.dataA = [{image:"assets/kitten1.jpg"}, {image: "assets/kitten2.jpg"}, {image: "assets/kitten3.jpg"}, {image:"assets/lion1.jpeg"}, {image:"assets/lion2.jpeg"}, {image:"assets/lion3.jpeg"}, {image:"assets/tiger1.jpeg"} ];
 
-    $scope.dataB = [{text: 'Bill'}, {text: 'Jane'}, {text: 'Alex'}, {text: 'Steve'}, {text: 'Bob'}, {text: 'Aaron'}, {text: 'Michael'}];
+  $scope.dataB = [{text: 'Bill'}, {text: 'Jane'}, {text: 'Alex'}, {text: 'Steve'}, {text: 'Bob'}, {text: 'Aaron'}, {text: 'Michael'}];
 
-    $scope.dataC = [{text: 'Lettuce'}, {text: 'Tomato'}, {text: 'Onion'}, {text: 'Turkey'}, {text: 'Wheat Bread'}, {text: 'Sprouts'}, {text: 'Avocado'}];
+  $scope.dataC = [{text: 'Lettuce'}, {text: 'Tomato'}, {text: 'Onion'}, {text: 'Turkey'}, {text: 'Wheat Bread'}, {text: 'Sprouts'}, {text: 'Avocado'}];
 
-  
   });
--- js/directives.js
+++ js/directives.js
@@ -1,6 +1,12 @@
 'use strict';
 /*global angular*/
 
+// The scroller directive, defined below, handles all of the infinite scroll functionality. 
+// The scrollability of the parent element--an unordered list -- is achieved with CSS, and the layout is established in the template html file.
+// The scroll behavior is set with ng-repeat, which will automatically iterate and append the entirety of whatever collection it is passed.
+// The specific scrollable contents are set each time an instance of the directive is called. 
+// This is the main benefit of using a directive for this purpose.
+
 angular.module('ScrollTest.directives', [])
 
   .directive('scroller', function () {
@@ -9,6 +15,18 @@ angular.module('ScrollTest.directives', [])
      templateUrl: 'partials/infinite_scroll.html',
      scope: {
       contents: '=contents'
+    },
+
+    // Adding an event listener to detect when a scroll has reached the end of a list of available elements
+    link: function (scope, element) {
+      element.on('scroll', function (event) {
+        var number = this.offsetHeight;
+        if (this.scrollTop === this.scrollHeight - this.offsetHeight) {
+          console.log('at Bottom');
+        }
+  
+      });
+
 
     }
 
